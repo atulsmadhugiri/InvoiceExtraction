87928fbe4e802584ef30c39f7ec2fcab612c1c68
Atul Madhugiri <atulm@berkeley.edu>
2024-10-11 11:02:16 -0400

    Make all the request-related types `Codable`

--- InvoiceExtractionService/InvoiceExtractionRequest.swift
+++ InvoiceExtractionService/InvoiceExtractionRequest.swift
@@ -1,37 +1,37 @@
 import Foundation
 
-struct GenerationConfig {
+struct GenerationConfig: Codable {
   let temperature: Double
   let maxOutputTokens: Int
   let topP: Double
   let seed: Int
 }
 
-struct SafetySetting {
+struct SafetySetting: Codable {
   let category: String
   let threshold: String
 }
 
-struct InlineData {
+struct InlineData: Codable {
   let mimeType: String
   let data: String
 }
 
-struct TextPrompt {
+struct TextPrompt: Codable {
   let text: String
 }
 
-struct Part {
+struct Part: Codable {
   let inlineData: InlineData?
   let text: String?
 }
 
-struct RequestContents {
+struct RequestContents: Codable {
   let role: String
   let parts: [Part]
 }
 
-struct Request {
+struct Request: Codable {
   let contents: RequestContents
   let generationConfig: GenerationConfig
   let safetySettings: [SafetySetting]
