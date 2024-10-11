dea3f145a82750773e2951d636f587d6946b59af
Atul Madhugiri <atulm@berkeley.edu>
2024-10-11 10:44:14 -0400

    Bring in `NetworkUtils.sendNetworkRequest`
    Grabbed this from ChooChoo which I grabbed from CruiseControl which I grabbed from Scribble or something.

---
+++ InvoiceExtractionService/NetworkManager.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct NetworkUtils {
+  static func sendNetworkRequest(
+    to endpoint: String
+  ) async throws -> Data {
+    var request = URLRequest(url: URL(string: endpoint)!)
+    request.httpMethod = "GET"
+
+    let (data, response) = try await URLSession.shared.data(for: request)
+    guard let httpResponse = response as? HTTPURLResponse,
+      (200...299).contains(httpResponse.statusCode)
+    else {
+      throw URLError(.badServerResponse)
+    }
+    return data
+  }
+}
