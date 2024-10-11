7055b5836b25cedb987f19338c6b2ea21c2194b0
Atul Madhugiri <atulm@berkeley.edu>
2024-10-11 10:22:25 -0400

    Introduce `GenerationConfig`

---
+++ InvoiceExtractionService/InvoiceExtractionRequest.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct GenerationConfig {
+  let temperature: Double
+  let maxOutputTokens: Int
+  let topP: Double
+  let seed: Int
+}
--- InvoiceExtractionService/main.swift
+++ InvoiceExtractionService/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  InvoiceExtractionService
-//
-//  Created by atul on 10/11/24.
-//
-
 import Foundation
 
 print("Hello, World!")
-
