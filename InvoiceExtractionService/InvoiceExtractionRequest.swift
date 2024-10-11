100b55cff93cd544df74a21cee5e75cb1e6054c9
Atul Madhugiri <atulm@berkeley.edu>
2024-10-11 10:33:43 -0400

    Add rest of types to `InvoiceExtractionRequest`

--- InvoiceExtractionService/InvoiceExtractionRequest.swift
+++ InvoiceExtractionService/InvoiceExtractionRequest.swift
@@ -6,3 +6,33 @@ struct GenerationConfig {
   let topP: Double
   let seed: Int
 }
+
+struct SafetySetting {
+  let category: String
+  let threshold: String
+}
+
+struct InlineData {
+  let mimeType: String
+  let data: String
+}
+
+struct TextPrompt {
+  let text: String
+}
+
+struct Parts {
+  let inlineData: InlineData?
+  let text: String?
+}
+
+struct RequestContents {
+  let role: String
+  let parts: [Parts]
+}
+
+struct Request {
+  let contents: RequestContents
+  let generationConfig: GenerationConfig
+  let safetySettings: [SafetySetting]
+}
